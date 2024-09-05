2aeee28eaf62a4cfb396e398f6ce98475fb7984f
Prasanth <sekaranp92@gmail.com>
2024-09-05 20:07:29 +0000

    rename main.c to server_sample. Add client_sampel file

---
+++ client_sample.c
@@ -0,0 +1,36 @@
+#include <sys/types.h> //size_t
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netdb.h>
+#include "getaddr.h"
+
+
+int main(int argc, char* argv[]){
+    char* value = argv[1];
+    printf("input: %s", value);
+    int server_or_client = 0; //AI_PASSIVE - server;  0 - client
+    struct addrinfo* result = getaddr(argv[1], server_or_client);
+    int sock; 
+
+    sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    printf("socket fd: %d\n", sock);
+    if (argc < 2 && server_or_client == AI_PASSIVE) {
+        bind(sock, result->ai_addr, result->ai_addrlen);
+    }
+
+    printf("%d" , in6addr_any.s6_addr[0]);
+
+    
+    /* OLD WAY OF PACKING SOCKADDR_IN FOR BINDING. USE getaddrinfo()
+    struct sockaddr_in my_addr; 
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(8000);
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY==0.0.0.0
+    memset(my_addr.sin_zero, '\0', sizeof my_addr.sin_zero);
+    
+    bind(sock, (struct sockaddr*)&my_addr, sizeof my_addr);
+    */
+
+    freeaddrinfo(result);
+}
