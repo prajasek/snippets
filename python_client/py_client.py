69666bcd6c0912b9cabc32474afa96f304c4611d
Prasanth <sekaranp92@gmail.com>
2024-09-07 01:15:44 +0000

    testing sending file back and forth w/python

--- python_client/py_client.py
+++ python_client/py_client.py
@@ -1,17 +1,36 @@
 import socket 
 from random import randint
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("connecting")
-sock.connect(("localhost", 8080))
+x= sock.connect(("localhost", 8080))
 
 while (True):
     strng = input("Input a string to send: ")
     if (strng == 'quit' or strng == 'Quit'):
         break
     raw = bytes(strng, 'utf-8')
-    print(raw)
     sock.send(raw)
+    print(">> ", raw)
+    data = b""
+    
+    EOF = False
+    while not EOF:
+        response=sock.recv(1000)
+        if not response:  # server closed connection
+            break
+        if b"----------xyzEOF" in response:  
+            print("---- ", response)
+            EOF=True
+            break
+        data += response 
+        print ("---- ", response)
+    if EOF: 
+        file = open("output.txt", 'ab')
+        file.write(data)
+        file.close()
+        break
 
 sock.shutdown(socket.SHUT_RDWR)
 sock.close()
